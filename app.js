81dd766a233ceb4a2f6b0ae2901855f57ac8c07f
Armando <cuelarmando@yahoo.com>
2022-01-25 19:05:10 -0500

    added inquirer

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
+
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html yo see the output!')
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
